e18a13a6db8080bf665d96fa6b6d54093420f9d9
André R. B <bronca.andre@gmail.com>
2022-10-31 08:54:19 -0300

    Mudando a referência de beans para arquivo xml

--- spring-book-ch2/src/main/java/com/wiley/beginning/spring/ch2/Main.java
+++ spring-book-ch2/src/main/java/com/wiley/beginning/spring/ch2/Main.java
@@ -1,26 +1,31 @@
 package com.wiley.beginning.spring.ch2;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.scheduling.annotation.AsyncAnnotationAdvisor;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext appContext = 
-				new AnnotationConfigApplicationContext(Ch2BeanConfiguration.class);
+		double amount = 5.30;
+		String xml_file = "/com/wiley/beginning/spring/ch2/ch2-beans.xml";
+//		AnnotationConfigApplicationContext appContext = 
+//				new AnnotationConfigApplicationContext(Ch2BeanConfiguration.class);
+//		
+		ClassPathXmlApplicationContext appContext = 
+				new ClassPathXmlApplicationContext(xml_file);
 	
 		AccountService as = 
-				appContext.getBean("accountService2", AccountService.class);
+				appContext.getBean("accountService", AccountService.class);
 	
 		System.out.println("Before money transfer");
 		System.out.println("Account 1 balance: "+ as.getAccount(1).getBalance());
 		System.out.println("Account 2 balance: "+ as.getAccount(2).getBalance());
 		
-		as.transferMoney(1, 2, 5.25);
+		as.transferMoney(1, 2, amount);
 		
-		System.out.println("After money transfer");
+		System.out.println("After money transfer: "+ amount);
 		System.out.println("Account 1 balance: "+ as.getAccount(1).getBalance());
 		System.out.println("Account 2 balance: "+ as.getAccount(2).getBalance());
+		
+		appContext.close();
 		
 	}
 }
